f4f169a087b3c5ba9de84638594acbe59fb09b94
BerkGokkaya <berk5900@hotmail.com>
2026-06-24 14:54:24 +0300

    model sınıfları eklendi

---
+++ OrbitalTracker/Models/TleData.cs
@@ -0,0 +1,9 @@
+﻿namespace OrbitalTracker.Models
+{
+    public class TleData
+    {
+        public string Name { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+    }
+}
